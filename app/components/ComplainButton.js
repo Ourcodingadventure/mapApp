5e958b8a872ada9acf7bd86f34cdbc10ae4da48e
Anya Joy <urda-a@mail.ru>
2022-01-11 12:17:09 +0200

    tab icons

--- app/components/ComplainButton.js
+++ app/components/ComplainButton.js
@@ -1,36 +1,31 @@
 import React from "react";
 import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import Colors from "../config/Colors";
 
 function ComplainButton({ onPress }) {
-    return (
-        <TouchableOpacity onPress={onPress}>
-            <View style={styles.container}>
-                <MaterialCommunityIcons
-                    name="plus-circle"
-                    color={'white'}
-                    size={40}
-                />
-
-            </View>
-
-        </TouchableOpacity>
-    );
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.container}>
+        <Text style={styles.plus}>+</Text>
+      </View>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: "center",
-        backgroundColor: Colors.primaryLight,
-        borderColor: 'white',
-        borderRadius: 40,
-        borderWidth: 10,
-        bottom: 30,
-        height: 80,
-        justifyContent: "center",
-        width: 80,
-    },
+  container: {
+    alignItems: "center",
+    backgroundColor: Colors.pink,
+    borderRadius: 10,
+    top: 5,
+    height: 36,
+    justifyContent: "center",
+    width: 80,
+  },
+  plus: {
+    alignItems: "center",
+    color: Colors.semiTransparentWhite,
+  },
 });
 
 export default ComplainButton;
