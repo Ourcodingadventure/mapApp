7e951dcf195c09c7a88bde8c33be7069c7e9b304
Anya Joy <urda-a@mail.ru>
2022-01-11 16:45:38 +0200

    Upd postcomplain screen set up

--- app/navigation/post-navigation.js/PostNavigation.js
+++ app/navigation/post-navigation.js/PostNavigation.js
@@ -1,22 +1,19 @@
-import React from 'react';
+import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import PostComplainScreen from '../../screens/PostComplainScreen'
+import PostComplainScreen from "../../screens/PostComplainScreen";
 const Stack = createStackNavigator();
 
-
-
 const PostNavigation = () => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name='PostComplain' component={PostComplainScreen}
-                options={{
-                    headerShown: false
-                }
-                }
-            />
-
-        </Stack.Navigator>
-    )
-
-}
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="PostComplain"
+        component={PostComplainScreen}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
 export default PostNavigation;
