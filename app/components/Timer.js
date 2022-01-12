090dd8d42a66186ecffeb270db0ca86e3bbf3a22
Ourcodingadventure <Javitan0309@gmail.com>
2022-01-12 20:37:23 +0200

    .

--- app/components/Timer.js
+++ app/components/Timer.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Text, View } from "react-native";
+import AppText from "./text/AppText";
 // import AppButton from "./Button";
 
 function Timer({ timer }) {
   console.log(timer);
   return (
     <View style={{ display: "flex", flexDirection: "row" }}>
-      <Text>Trip Duration</Text>
-      <Text> {("0" + Math.floor((timer / 60000) % 60)).slice(-2)}:</Text>
-      <Text> {("0" + Math.floor((timer / 1000) % 60)).slice(-2)}:</Text>
-      <Text> {("0" + Math.floor((timer / 10) % 100)).slice(-2)}</Text>
+      <AppText>Trip Duration</AppText>
+      <AppText> {("0" + Math.floor((timer / 60000) % 60)).slice(-2)}:</AppText>
+      <AppText> {("0" + Math.floor((timer / 1000) % 60)).slice(-2)}:</AppText>
+      <AppText> {("0" + Math.floor((timer / 10) % 100)).slice(-2)}</AppText>
       {/* <View>
         {!timerOn && (
           <AppButton onPress={() => setTimerOn(true)}>start</AppButton>
